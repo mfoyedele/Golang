e4fa3901f045026f79975e39d7fa3db2cf87f428
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-08-22 19:40:00 +0000

    Create Variables.go

---
+++ Variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 7
+
+	fmt.Println(a)
+
+	b := "hello world"
+	fmt.Println(b)
+
+	var c int = 10
+	fmt.Println(c)
+
+	var d string = "Education is the best legacy"
+	fmt.Println(d)
+}
